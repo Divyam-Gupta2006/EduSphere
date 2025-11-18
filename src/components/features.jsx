80f1aedce37207c20f3829f70dd25686b7e680b9
Divyam-Gupta2006 <divyamguptaofficalpostbox@gmail.com>
2025-11-19 02:00:38 +0530

    Delete src/components/features.jsx

--- src/components/features.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function Features() {
-	return (
-		<section className="px-10 py-20 bg-blue-50 transition-colors text-center">
-			<h2 className="text-3xl font-bold text-blue-700 mb-8">Features Coming Soon</h2>
-			<p className="text-lg text-gray-700">Stay tuned for more NEP-compliant features and tools for students, colleges, and companies.</p>
-		</section>
-	);
-}
